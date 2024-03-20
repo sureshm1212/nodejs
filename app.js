cd9f72f93022a63333988165adf2322be8496667
Suresh M <sureshm1212@gmail.com>
2024-03-21 03:15:13 +0530

    mar2024 # new api

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 });
 
+app.get('/greetings/:name', function (req, res) {
+   res.send(`Welcome ${req.params.name}`);
+});
+
  var server = app.listen(8080, function () {
    console.log("Express App running at http://127.0.0.1:8080/");
 });
