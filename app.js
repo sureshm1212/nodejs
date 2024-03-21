6a7fee138c1f4e3d36d4339a15f79ced28573983
Suresh M <7118004+sureshm1212@users.noreply.github.com>
2024-03-21 09:21:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 });
 
+app.get('/greeting', function (req, res) {
+   res.send('Hello World');
+});
  var server = app.listen(8080, function () {
    console.log("Express App running at http://127.0.0.1:8080/");
 });
